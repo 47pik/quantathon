18896929a9c68cdf4d9d942df718effa5674faa3
Carter <carter.tinney@live.ca>
2016-03-18 23:02:46 -0400

    did p1

---
+++ p1.py
@@ -0,0 +1,32 @@
+import numpy as np
+from metrics import *
+import time
+import sys
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    ret_pf = [0, 0]
+    st = time.time()
+    for t in range(2, rd.T):
+        rp = RP1(t)
+        ret_pf.append(rp)
+    
+    plt.plot(ret_pf)
+    plt.show()
+    
+    cumR = []
+    rp2 = [x + 1 for x in ret_pf]
+    for t in range(2, rd.T):    
+        cumR.append(np.log(np.prod(rp2[2: t + 1])))
+    
+    eqLO = []
+    rccs = [np.mean([RCC(t, j) for j in rd.stock_dict]) + 1 for t in range(rd.T)]
+    for t in range(rd.T):
+        eqLO.append(np.log(np.prod(rccs[2: t + 1])))
+        
+    #write csv
+    w = csv.writer('p1.csv')
+        
+        
+
